c43e00c26029f2e99c375aa514e3a74dcc3e555c
karamalesa1980 <karamalesa@gmail.com>
2019-06-06 20:46:03 +0300

    add products to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+100.times do
+
+# . . .
+Product.create(title: 'Programming Ruby 1.9', description: %{<p>
+Ruby is th e f a s t e s t growing and most e x c it in g dynamic language
+out th e re . I f you need to get working programs d e liv e re d f a s t ,
+you should add Ruby to your to o lb o x .
+</p>}, image_url: 'ruby.jpg', price: 49.95)
+# . . .
+end
